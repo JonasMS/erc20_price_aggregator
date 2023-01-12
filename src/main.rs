db88d3351b4af907388b1e8bf13ae6dae5f1d1e7
JonasMS <jonas.sota@gmail.com>
2023-01-12 14:06:44 -0700

    make main() async

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use price_aggregator::config::get_config;
 
-fn main() {
+#[async_std::main]
+async fn main() {
     let config = get_config();
 
     println!("CONFIG: {:?}", config);
 
-    price_aggregator::run(config);
+    price_aggregator::run(config).await;
 }

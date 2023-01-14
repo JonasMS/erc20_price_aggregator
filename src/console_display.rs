b175501a62ae68219d67e28cf5530a61fd6bf433
JonasMS <jonas.sota@gmail.com>
2023-01-14 12:47:09 -0700

    print table lables

--- src/console_display.rs
+++ src/console_display.rs
@@ -1,3 +1,21 @@
 use crate::exchange::ExchangeRate;
+use prettytable::{Attr, Cell, Row, Table};
 
-pub fn print_rate_table(exchange_rates: Vec<ExchangeRate>) -> Result<(), std::error::Error> {}
+pub fn print_rate_table(exchange_rates: Vec<ExchangeRate>) -> () {
+    let mut table = Table::new();
+
+    /* Generate Table Labels */
+    let labels = vec!["ID", "Token In", "Token Out", "Rate", "Exchange", "Network"];
+    let mut label_cells: Vec<Cell> = Vec::new();
+
+    for label in labels {
+        label_cells.push(Cell::new(label).with_style(Attr::Bold));
+    }
+
+    table.add_row(Row::new(label_cells));
+
+    /* Generate Table Body */
+    // for rate in exchange_rates {}
+
+    table.printstd();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 use crate::config::Config;
+use crate::console_display::print_rate_table;
 use crate::exchange::get_exchange_rates;
 use crate::rate_query_factory::get_rate_queries;
 
 pub mod config;
+mod console_display;
 mod exchange;
 mod rate_query_factory;
+
+#[macro_use]
+extern crate prettytable;
 
 pub async fn run(config: Config) -> () {
     println!("EXECUTING run()");
@@ -14,6 +19,7 @@ pub async fn run(config: Config) -> () {
 
     let (exchange_rates, errors) = get_exchange_rates(&rate_queries).await;
 
-    println!("EXCHANGE RATES: {:?}", exchange_rates);
-    println!("ERRORS: {:?}", errors);
+    // println!("EXCHANGE RATES: {:?}", exchange_rates);
+    // println!("ERRORS: {:?}", errors);
+    print_rate_table(exchange_rates);
 }

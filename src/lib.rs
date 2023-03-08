75d0960a35b5b4d0b29ebf116b4eaf1a0d678b4b
JonasMS <jonas.sota@gmail.com>
2023-03-08 14:33:12 -0700

    clean code

--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,9 @@ mod rate_query_factory;
 extern crate prettytable;
 
 pub async fn run(config: Config) -> () {
-    println!("EXECUTING run()");
-
     let rate_queries = get_rate_queries(&config.token_pairs);
-    println!("RATE QUERIES: {:?}", rate_queries);
 
-    let (exchange_rates, errors) = get_exchange_rates(&rate_queries).await;
+    let (exchange_rates, _errors) = get_exchange_rates(&rate_queries).await;
 
-    // println!("EXCHANGE RATES: {:?}", exchange_rates);
-    // println!("ERRORS: {:?}", errors);
     print_rate_table(exchange_rates);
 }
